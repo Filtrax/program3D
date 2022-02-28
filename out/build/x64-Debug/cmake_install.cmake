98464d56d17dc7ad671e36ed0fc23593785eafa8
Filip Piłat <filippilat@users.noreply.com>
2022-02-28 23:48:07 +0100

    Ostatni zestaw zadań

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -44,7 +44,27 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("D:/Studia/programowanie3D/3d-main/3d-main/out/build/x64-Debug/_deps/spdlog-build/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("D:/Studia/programowanie3D/3d-main/3d-main/out/build/x64-Debug/src/Application/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Studia/programowanie3D/3d-main/3d-main/out/build/x64-Debug/src/ObjectReader/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Studia/programowanie3D/3d-main/3d-main/out/build/x64-Debug/src/XeEngine/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Studia/programowanie3D/3d-main/3d-main/out/build/x64-Debug/src/Engine/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
@@ -55,6 +75,11 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
   include("D:/Studia/programowanie3D/3d-main/3d-main/out/build/x64-Debug/src/Assignments/Uniforms/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Studia/programowanie3D/3d-main/3d-main/out/build/x64-Debug/src/Assignments/Phong/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
